16ab1a6db55e352261d11ab01660cd888f67afc4
Paulo Victor Mesquita <victor.mesquita@trutec.com.br>
2022-08-12 13:50:06 -0300

    dashboard

--- core/resources/views/templates/basic/user/transactions.blade.php
+++ core/resources/views/templates/basic/user/transactions.blade.php
@@ -3,6 +3,39 @@
 @section('panel')
     <div class="row">
 
+        <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+            <div class="dashboard-w1 bg--cyan b-radius--10 box-shadow">
+                <div class="icon">
+                    <i class="las la-money-bill-wave"></i>
+                </div>
+                <div class="details">
+                    <div class="numbers">
+                        <span class="currency-sign">{{$general->cur_text}}</span>
+                        <span class="amount">{{getAmount(auth()->user()->total_invest)}}</span>
+                    </div>
+                    <div class="desciption">
+                        <span class="text--small">@lang('Total Investido')</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+            <div class="dashboard-w1 bg--12 b-radius--10 box-shadow">
+                <div class="icon">
+                    <i class="las la-money-bill"></i>
+                </div>
+                <div class="details">
+                    <div class="numbers">
+                        <span class="currency-sign">{{$general->cur_text}}</span>
+                        <span class="amount">{{getAmount(auth()->user()->total_ref_com)}}</span>
+                    </div>
+                    <div class="desciption">
+                        <span class="text--small">@lang('Bonus por Indicação')</span>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-lg-12">
             <div class="card b-radius--10 ">
                 <div class="card-body p-0">
@@ -21,6 +54,7 @@
                             </thead>
                             <tbody>
                             @forelse($transactions  as $trx)
+
                                 <tr>
                                     <td data-label="@lang('SL')">{{ $transactions->firstItem()+$loop->index }}</td>
                                     <td data-label="@lang('Data')">{{ showDateTime($trx->created_at) }}</td>
@@ -55,7 +89,8 @@
 @push('breadcrumb-plugins')
     <form action="" method="GET" class="form-inline float-sm-right bg--white">
         <div class="input-group has_append">
-            <input type="text" name="search" class="form-control" placeholder="@lang('Buscar Por Transação')" value="{{ $search ?? '' }}">
+            <input type="text" name="search" class="form-control" placeholder="@lang('Buscar Por Transação')"
+                   value="{{ $search ?? '' }}">
             <div class="input-group-append">
                 <button class="btn btn--primary" type="submit"><i class="fa fa-search"></i></button>
             </div>
